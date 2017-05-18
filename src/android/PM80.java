4ca5d3091e7c9c021e8c55b6a93a347d3b623595
brian <brian.hollander@coolprofs.com>
2017-05-18 18:24:17 +0200

    Changes in config

--- src/android/PM80.java
+++ src/android/PM80.java
@@ -5,6 +5,7 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
+import org.json.*;
 
 import device.common.MsrIndex;
 import device.common.MsrResult;
